64989c76e8063f84c491d7dac44c30adc0cab96f
davidreese3 <david33reese@gmail.com>
2025-05-07 20:19:13 -0400

    Setup for tickets (controller + objs)

---
+++ src/main/java/com/thesis/recipease/web/TicketController.java
@@ -0,0 +1,14 @@
+package com.thesis.recipease.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class TicketController {
+    /*
+    @Autowired
+    private TicketValidator ticketValidator;
+    @Autowired
+    private TicketSanitizer ticketSanitizer;
+    */
+}
